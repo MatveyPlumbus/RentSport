e7bde0ee6452ebc652f5bfc3ee1f00bbd68465a7
Matvey Plumbus <matveyplumbus@gmail.com>
2024-12-03 15:14:02 +0300

    Добавлен проект Docusaurus

--- website-RentSport/docusaurus.config.js
+++ website-RentSport/docusaurus.config.js
@@ -30,18 +30,115 @@ const config = {
       },
     ],
   ],
+  //подключаем плагин для drawio
   plugins: [
+    ['drawio', {}]
+  ],
+  presets: [
     [
-      'docusaurus-plugin-drawio',
-      {
-        drawio: {
-          drawioPath: './drawio-files',
-          outputPath: './static/img/drawio',
-          outputFormat: 'svg',
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: 'docs',
+          editUrl:
+            'https://github.com/itikineva/doc-as-a-code-workshop',
+          remarkPlugins: [simplePlantUML], //подключаем плагин для plantuml
         },
-      },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+    // подключаем плагин для OPENAPI
+    [
+      'redocusaurus',
+      {
+        specs: [
+          {
+            id: 'petstore',
+            spec: 'api_specs/openapi.yaml',
+          },
+        ],
+        theme: {
+          primaryColor: '#1890ff',
+        },
+      }
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'doc-as-a-code-workshop', //Название на навбаре
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg', //Логотип на навбаре
+        },
+        // тут можно настроить элементы навбара
+        items: [
+          {
+            href: 'https://github.com/itikineva/doc-as-a-code-workshop',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      // тут можно настроить элементы футера
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Docs',
+                to: '/docs/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
 
 export default config;
